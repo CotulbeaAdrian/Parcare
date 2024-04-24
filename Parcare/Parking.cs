e23aade2d8e15c4793aa1b3ff5e1b7310c15af53
CotulbeaAdrian <cotulbeaadrianionut@gmail.com>
2024-04-24 11:55:45 +0300

    Creathed parkingEntry method

--- Parcare/Parking.cs
+++ Parcare/Parking.cs
@@ -8,5 +8,27 @@ namespace Parcare
 {
     internal class Parking
     {
+        private int availableSlots;
+        private DateTime entryTime;
+
+        public Parking(int totalSlots) {
+            availableSlots = totalSlots;
+        }
+
+        public bool parkingEntry()
+        {
+            if (availableSlots > 0)
+            {
+                entryTime = DateTime.Now;
+                Console.WriteLine($"Parking entered at {entryTime}");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"There are no empty slots at the time. Please come back later!");
+                return false;
+            }
+        }
+
     }
 }
